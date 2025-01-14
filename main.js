74a4c471b376d2fd6aab14e66edc7dfa6f6df8a8
Torjus <Torjus24sfj@gmail.com>
2025-01-14 10:32:16 +0100

    logic

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const chessboard = document.getElementById('chessboard');
+    const tileSize = 50;
 
     for (let row = 0; row < 8; row++) {
         for (let col = 0; col < 8; col++) {
@@ -14,5 +15,32 @@ document.addEventListener('DOMContentLoaded', () => {
             chessboard.appendChild(tile)
         }
     }
+
+    let pawnX = 0;
+    let pawnY = 0;
+
+    pawn.style.left = `${pawnX * tileSize + 5}px`
+    pawn.style.right = `${pawnY * tileSize + 5}px`
+
+    let isDragging = false
+
+    pawn.addEventListener('mousedown', (e) => {
+        isDragging = true;
+    })
+
+    document.addEventListener('mousemove', (e) => {
+        if (isDragging) {
+            pawn.style.left = `${e.pageX - 20}px`;
+            pawn.style.top = `${e.pageY - 20}px`;
+        }
+    })
+
+    document.addEventListener('mouseup', (e) => {
+        if(isDragging){
+            isDragging = false;
+
+            //TODO moving and snapping to tile
+        }
+    })
 });
 
