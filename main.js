709e7b58623b0c1f6424cf226c890f9f4953f48d
Torjus <Torjus24sfj@gmail.com>
2025-01-14 10:11:55 +0100

    board

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-const chessboard = document.getElementById('chessboard');
+document.addEventListener('DOMContentLoaded', () => {
+    const chessboard = document.getElementById('chessboard');
 
-for(let row = 0; row < 8; row++){
-    for(let col = 0; col < 8; col++){
-        const tile = document.createElement('div')
-        tile.classList.add('tile')
-        if(row + col % 2 === 0){
-            tile.classList.add('light')
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            const tile = document.createElement('div')
+            tile.classList.add('tile')
+            if ((row + col) % 2 === 0) {
+                tile.classList.add('light')
+            }
+            else {
+                tile.classList.add('dark')
+            }
+            chessboard.appendChild(tile)
         }
-        else
-        {
-            tile.classList.add('dark')
-        }
-        chessboard.appendChild('tile')
     }
-}
+});
+
