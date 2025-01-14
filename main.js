a6776eaa3046ffa60e03db7d628afa42710f74c6
Torjus <Torjus24sfj@gmail.com>
2025-01-14 09:41:31 +0100

    initial

---
+++ main.js
@@ -0,0 +1,16 @@
+const chessboard = document.getElementById('chessboard');
+
+for(let row = 0; row < 8; row++){
+    for(let col = 0; col < 8; col++){
+        const tile = document.createElement('div')
+        tile.classList.add('tile')
+        if(row + col % 2 === 0){
+            tile.classList.add('light')
+        }
+        else
+        {
+            tile.classList.add('dark')
+        }
+        chessboard.appendChild('tile')
+    }
+}
